8f2077a48655ad21dfd53f9a0ebd873c6fc3af92
Leo <81168288+Zare22@users.noreply.github.com>
2023-06-05 18:34:47 +0200

    Only admin can create quiz

--- app/src/main/java/hr/algebra/surfsafely/fragment/QuizFragment.kt
+++ app/src/main/java/hr/algebra/surfsafely/fragment/QuizFragment.kt
@@ -1,9 +1,11 @@
 package hr.algebra.surfsafely.fragment
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import hr.algebra.surfsafely.R
@@ -12,21 +14,36 @@ import hr.algebra.surfsafely.databinding.FragmentQuizBinding
 import hr.algebra.surfsafely.framework.replaceFragment
 import hr.algebra.surfsafely.framework.showToast
 import hr.algebra.surfsafely.service.ApiService
+import hr.algebra.surfsafely.viewmodel.UserViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.activityViewModel
 
+@RequiresApi(Build.VERSION_CODES.O)
 class QuizFragment : Fragment() {
 
     private lateinit var binding: FragmentQuizBinding
+    private val userViewModel by activityViewModel<UserViewModel>()
     private val apiService by inject<ApiService>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentQuizBinding.inflate(inflater, container, false)
+        initCreateButton()
+        loadQuizzes()
+        binding.btnCreateQuiz.setOnClickListener {
+            this.requireActivity().replaceFragment(R.id.main_fragment_container, CreateQuizFragment(), true)
+        }
+        return binding.root
+    }
 
+
+    private fun initCreateButton() { if (userViewModel.user.value?.roleId == 2L) binding.btnCreateQuiz.visibility = View.GONE }
+
+    private fun loadQuizzes() {
         lifecycleScope.launch {
             try {
                 val response = withContext(Dispatchers.IO) { apiService.getAllQuizzes().execute() }
@@ -38,16 +55,11 @@ class QuizFragment : Fragment() {
                         }
                         binding.quizRecyclerView.adapter = adapter
                     }
-                } else {
+                } else
                     activity?.showToast("Couldn't fetch our quizzes!")
-                }
             } catch (e: Exception) {
                 activity?.showToast("Unexpected error!")
             }
         }
-        binding.btnCreateQuiz.setOnClickListener {
-            this.requireActivity().replaceFragment(R.id.main_fragment_container, CreateQuizFragment(), true)
-        }
-        return binding.root
     }
 }
