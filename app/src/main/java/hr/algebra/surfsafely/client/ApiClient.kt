5683804ef4c315d8f2e9ee7a65760e28aaf00110
Leo <81168288+Zare22@users.noreply.github.com>
2023-06-09 01:22:47 +0200

    Base_URL change

--- app/src/main/java/hr/algebra/surfsafely/client/ApiClient.kt
+++ app/src/main/java/hr/algebra/surfsafely/client/ApiClient.kt
@@ -12,7 +12,7 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 
 object ApiClient {
 
-    private const val BASE_URL = "http://10.0.2.2:8080/api/"
+    private const val BASE_URL = "http://10.0.2.2:8081/api/"
     private val moshi = Moshi.Builder()
         .addLast(KotlinJsonAdapterFactory())
         .build()
