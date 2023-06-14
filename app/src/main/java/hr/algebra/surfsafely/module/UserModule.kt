5831a544cc8dbfbdfd7df875253f5cd3ccb1aa8d
Leo <81168288+Zare22@users.noreply.github.com>
2023-06-14 15:30:04 +0200

    Update UserModule.kt

--- app/src/main/java/hr/algebra/surfsafely/module/UserModule.kt
+++ app/src/main/java/hr/algebra/surfsafely/module/UserModule.kt
@@ -1,9 +1,12 @@
 package hr.algebra.surfsafely.module
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import hr.algebra.surfsafely.viewmodel.UserViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
+@RequiresApi(Build.VERSION_CODES.O)
 val userModule = module {
     viewModel { UserViewModel(get(), get()) }
 }
